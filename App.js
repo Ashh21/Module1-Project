22335b14e5b458041d752d01a55170eeaa5cc60c
Ashh21 <ash.shrivastav0@gmail.com>
2023-08-30 17:40:06 +0530

    modified the code and do some changes

--- App.js
+++ App.js
@@ -2,18 +2,23 @@ const choices = ["rock", "paper", "scissors"];
 const buttons = document.querySelectorAll(".choices");
 const rulesButton = document.getElementById("rules");
 const rulesPopup = document.getElementById("rules-popup"); 
-const closeButton = document.getElementById("close")
+const closeButton = document.getElementById("close");
 
-;let userScore = 0;
+
+let userScore = 0;
 let computerScore = 0;
 
+let compPicked = ""
+
+let result = ""
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
       const userChoice = button.id;
       const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-      let result = "";
-  
+      compPicked = computerChoice
+
+      console.log({userChoice,computerChoice})
       if (userChoice === computerChoice) {
         result = "Tie up!";
         localStorage.setItem("userScore", userScore);
@@ -31,8 +36,7 @@ buttons.forEach((button) => {
         localStorage.setItem("userScore", userScore);
         localStorage.setItem("computerScore", computerScore);
       }
-  
-      // document.getElementById("res").innerText = `You chose ${userChoice}. Computer chose ${computerChoice}`
+
 
         document.getElementById("userScore").innerHTML = ` ${userScore}`;
         document.getElementById("computerScore").innerHTML = ` ${computerScore}`;
@@ -41,14 +45,12 @@ buttons.forEach((button) => {
   });
 
   const savedScore = localStorage.getItem("userScore");
-  const savedScore1 = localStorage.getItem("computerScore")
+  const compScore = localStorage.getItem("computerScore")
   if (savedScore != null){
     userScore = savedScore;
-    userScore = savedScore;
-    computerScore = savedScore1;
+    computerScore = compScore;
     document.getElementById("userScore").innerHTML = ` ${userScore}`;
     document.getElementById("computerScore").innerHTML = ` ${computerScore}`;
-
   }
 
   rulesPopup.style.display = "none"
@@ -63,30 +65,66 @@ buttons.forEach((button) => {
 
 
 
-// const rock = document.getElementById("rock")
-// const scissor = document.getElementById("scissor")
-// const paper = document.getElementById("paper")
+const rock = document.getElementById("rock")
+const scissor = document.getElementById("scissors")
+const paper = document.getElementById("paper")
 
-// rock.addEventListener("click", () => {
-//   console.log("button clicked");
-// if(result === "You Win Against PC!"){
-// document.location.href = "Win.html";
-// document.getElementById("result").innerText = `${result}`;
-// } 
-// })
 
-// scissor.addEventListener("click", () => {
-//   console.log("button clicked");
-// if(result === "You Lost Against PC!"){
-// document.location.href = "Lost.html";
-// document.getElementById("result").innerText = `${result}`;
-// } 
-// })
 
-// paper.addEventListener("click", () => {
-//   console.log("button clicked");
-// if(result === "Tie up!"){
-// document.location.href = "Tied.html";
-// document.getElementById("result").innerText = `${result}`;
-// } 
-// })
+function handleChoiceClick(choice) {
+  const isWon = result === "You Win Against PC!";
+  const isDraw = result === "Tie up!";
+  const gameResult = isDraw ? "Tie up!" : isWon ? "You Win Against PC!" : "You Lost Against PC!";
+  const resultPage = isDraw ? "Tied.html" : isWon ? "Win.html" : "Lost.html";
+  window.location.href = `${resultPage}?choice=${choice}&gameResult=${gameResult}&compChoice=${compPicked}`;
+}
+
+rock.addEventListener("click", () => {
+  handleChoiceClick("rock");
+});
+
+scissor.addEventListener("click", () => {
+  handleChoiceClick("scissors");
+});
+
+paper.addEventListener("click", () => {
+  handleChoiceClick("paper");
+});
+
+ 
+rock.addEventListener("click", () => {
+  const isWon = result === "You Win Against PC!";
+  const isDraw = result === "Tie up!";
+  const gameResult = isDraw ? "Tie up!" : isWon ? "You Win Against PC!":"You Lost Against PC!";
+  const resultPage = isDraw ? "Tied.html" : isWon ? "Win.html":"Lost.html";
+  window.location.href = `${resultPage}?choice=rock&gameResult=${gameResult}&compChoice=${compPicked}`;
+})
+
+scissor.addEventListener("click", () => {
+  const isWon = result === "You Win Against PC!";
+  const isDraw = result === "Tie up!";
+  const gameResult = isDraw ? "Tie up!" : isWon ? "You Win Against PC!":"You Lost Against PC!";
+  const resultPage = isDraw ? "Tied.html" : isWon ? "Win.html":"Lost.html";
+  window.location.href = `${resultPage}?choice=scissors&gameResult=${gameResult}&compChoice=${compPicked}`;
+})
+
+paper.addEventListener("click", () => {
+  const isWon = result === "You Win Against PC!";
+  const isDraw = result === "Tie up!";
+  const gameResult = isDraw ? "Tie up!" : isWon ? "You Win Against PC!":"You Lost Against PC!";
+  const resultPage = isDraw ? "Tied.html" : isWon ? "Win.html":"Lost.html";
+  window.location.href = `${resultPage}?choice=paper&gameResult=${gameResult}&compChoice=${compPicked}`;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ html/Result.js
@@ -0,0 +1,58 @@
+
+
+let queryString = window.location.search;
+
+queryString = queryString.substring(1);
+
+
+let queryParams = queryString.split('&');
+
+let params = {};
+
+
+for (let i = 0; i < queryParams.length; i++) {
+  let param = queryParams[i].split('=');
+  let key = decodeURIComponent(param[0]);
+  let value = decodeURIComponent(param[1]);
+  params[key] = value;
+}
+
+const rockImg = `<img src="/images/icons8-fist-67 1.png" />`
+const scissorImg = `<img src="/images/16.42.45.jpg" />`
+const paperImg =  `<img src="/images/icons8-hand-64 1.png" />`
+
+const youPicked = params.choice;
+const compPicked = params.compChoice;
+const gameResult = params.gameResult;
+
+const youPickedEle = document.getElementById("you-picked")
+const computerPickedEle = document.getElementById("computer-picked")
+
+youPickedEle.innerHTML = youPicked === "rock" ? rockImg : youPicked === "paper"? paperImg : scissorImg 
+
+computerPickedEle.innerHTML = compPicked === "rock" ? rockImg : compPicked === "paper" ? paperImg : scissorImg 
+
+const choices = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll(".choices");
+const rulesButton = document.getElementById("rules");
+const rulesPopup = document.getElementById("rules-popup"); 
+const closeButton = document.getElementById("close");
+const res = document.getElementById("result")
+
+
+let userScore = localStorage.getItem("userScore");
+let computerScore = localStorage.getItem("computerScore");
+console.log("userScore: " + userScore + " computerScore: "+computerScore)
+document.getElementById("userScore").innerHTML = ` ${userScore}`;
+document.getElementById("computerScore").innerHTML = ` ${computerScore}`;
+
+res.innerText = gameResult;
+
+
+rulesPopup.style.display = "none"
+  rulesButton.addEventListener("click", () => {
+    rulesPopup.style.display = "block";
+  })
+  closeButton.addEventListener("click", () => {
+    rulesPopup.style.display = "none";
+  })
